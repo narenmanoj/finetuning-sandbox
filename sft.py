ac87f502b86b512d7ea2d2d745c9b7486652d58e
Naren Sarayu Manoj <manoj.narens@gmail.com>
2026-01-27 22:11:29 -0500

    update

--- sft.py
+++ sft.py
@@ -67,6 +67,6 @@ def sft_microbatch_train_step(
     gradient_accumulation_steps: int,
     normalize_constant: float = 1.0,
 ) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
-    loss = -masked_normalize(policy_log_probs, response_mask, normalize_constant=normalize_constant) / gradient_accumulation_steps
+    loss = -masked_normalize(policy_log_probs, response_mask, normalize_constant=normalize_constant)
     loss.backward()
-    return (loss, {"Loss value": loss})
+    return (loss / gradient_accumulation_steps, {"Loss value": loss})
