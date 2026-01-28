55d3ddbd4bf38e9869ecca8cfe794ebb26044dcb
Naren Sarayu Manoj <manoj.narens@gmail.com>
2026-01-27 19:59:27 -0500

    beginning the sft part

--- sft.py
+++ sft.py
@@ -9,12 +9,15 @@ def tokenize_prompt_and_output(prompt_strs: List[str],
 ) -> Dict[str, torch.Tensor]:
     prompt_tokenized = tokenizer(prompt_strs)["input_ids"]
     output_tokenized = tokenizer(output_strs)["input_ids"]
-    stitched = [torch.tensor(prompt_tokenized[i] + output_tokenized[i]) for i in range(len(prompt_tokenized))]
-    stitched = pad_sequence(stitched, batch_first=True, padding_value=-1, padding_side="left")
+    stitched = [(torch.tensor(prompt_tokenized[i] + output_tokenized[i]), torch.tensor([0] * len(prompt_tokenized[i] + [1] * len(output_tokenized[i])))) for i in range(len(prompt_tokenized))]
+    tokens = [stitch[0] for stitch in stitched]
+    mask = [stitch[1] for stitch in stitched]
+    stitched = pad_sequence(tokens, batch_first=True, padding_value=-1, padding_side="left")
+    response = pad_sequence(mask, batch_first=True, padding_value=0, padding_side="left")
     breakpoint()
     ans = {
         "input_ids": torch.tensor(prompt_tokenized),
         "labels": torch.tensor(output_tokenized),
-        "response_mask": None,
+        "response_mask": response,
     }
     return ans
