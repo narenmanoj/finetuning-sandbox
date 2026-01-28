a8a7cde8279bf9c088a3b6746ba876d65d53851e
Naren Sarayu Manoj <manoj.narens@gmail.com>
2026-01-27 20:29:39 -0500

    beginning the sft part

--- sft.py
+++ sft.py
@@ -9,14 +9,22 @@ def tokenize_prompt_and_output(prompt_strs: List[str],
 ) -> Dict[str, torch.Tensor]:
     prompt_tokenized = tokenizer(prompt_strs)["input_ids"]
     output_tokenized = tokenizer(output_strs)["input_ids"]
-    stitched = [(torch.tensor(prompt_tokenized[i] + output_tokenized[i]), torch.tensor([0] * len(prompt_tokenized[i]) + [1] * len(output_tokenized[i]))) for i in range(len(prompt_tokenized))]
+    stitched = [(torch.tensor(prompt_tokenized[i] + output_tokenized[i]), 
+                 torch.tensor([0] * len(prompt_tokenized[i]) + [1] * len(output_tokenized[i]))) 
+                 for i in range(len(prompt_tokenized))]
     tokens = [stitch[0] for stitch in stitched]
     mask = [stitch[1] for stitch in stitched]
-    stitched = pad_sequence(tokens, batch_first=True, padding_value=-1, padding_side="left")
-    response = pad_sequence(mask, batch_first=True, padding_value=0, padding_side="left")
+
+    inputs = [tokenized[:-1] for tokenized in tokens]
+    labels_lst = [tokenized[1:] for tokenized in tokens]
+    mask = [tokenized[1:] for tokenized in mask]
+
+    input_ids = pad_sequence(inputs, batch_first=True, padding_value=0, padding_side="right")
+    labels = pad_sequence(labels_lst, batch_first=True, padding_value=0, padding_side="right")
+    mask = pad_sequence(mask, batch_first=True, padding_value=0, padding_side="right")
     ans = {
-        "input_ids": stitched[:, :-1],
-        "labels": stitched[:, 1:],
-        "response_mask": response,
+        "input_ids": input_ids,
+        "labels": labels,
+        "response_mask": mask,
     }
     return ans
