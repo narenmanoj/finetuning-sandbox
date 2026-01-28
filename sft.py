33007fad24d19758a6dd75147b045e55e015cbc9
Naren Sarayu Manoj <manoj.narens@gmail.com>
2026-01-27 19:51:11 -0500

    beginning the sft part

--- sft.py
+++ sft.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn.utils.rnn import pad_sequence
 from transformers import PreTrainedTokenizer
 from typing import List, Dict
 
@@ -8,7 +9,8 @@ def tokenize_prompt_and_output(prompt_strs: List[str],
 ) -> Dict[str, torch.Tensor]:
     prompt_tokenized = tokenizer(prompt_strs)["input_ids"]
     output_tokenized = tokenizer(output_strs)["input_ids"]
-    stitched = [prompt_tokenized[i] + output_tokenized[i] for i in range(len(prompt_tokenized))]
+    stitched = [torch.tensor(prompt_tokenized[i] + output_tokenized[i]) for i in range(len(prompt_tokenized))]
+    stitched = pad_sequence(stitched, batch_first=True, padding_value=0)
     breakpoint()
     ans = {
         "input_ids": torch.tensor(prompt_tokenized),
