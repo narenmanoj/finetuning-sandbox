bf158be95bb57d84bebe7f86ca0bdd182c46497b
Naren Sarayu Manoj <manoj.narens@gmail.com>
2026-01-27 19:46:51 -0500

    beginning the sft part

--- sft.py
+++ sft.py
@@ -6,12 +6,12 @@ def tokenize_prompt_and_output(prompt_strs: List[str],
                                output_strs: List[str],
                                tokenizer: PreTrainedTokenizer
 ) -> Dict[str, torch.Tensor]:
-    prompt_tokenized = [tokenizer.tokenize(pro)[:-1] for pro in prompt_strs]
-    output_tokenized = [tokenizer.tokenize(out)[:-1] for out in output_strs]
+    prompt_tokenized = torch.tensor(tokenizer(prompt_strs)["input_ids"])
+    output_tokenized = torch.tensor(tokenizer(output_strs)["input_ids"])
     breakpoint()
     ans = {
-        "input_ids": None,
-        "labels": None,
+        "input_ids": torch.tensor(prompt_tokenized),
+        "labels": torch.tensor(output_tokenized),
         "response_mask": None,
     }
     return ans
