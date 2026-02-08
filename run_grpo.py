2a7b395eafda90b3fc9e50e92553188db68b9282
Naren Sarayu Manoj <manoj.narens@gmail.com>
2026-02-08 16:11:24 -0500

    idk

--- run_grpo.py
+++ run_grpo.py
@@ -73,16 +73,17 @@ def train_one_epoch(model,
         for j in range(n_train_steps):
             macrobatch_start = hyperparams["train_batch_size"] * j
             macrobatch_end = hyperparams["train_batch_size"] * (j + 1)
-            
-            for k in range(microbatch_size):
-                microbatch_start = macrobatch_start + microbatch_size * k * hyperparams["group_size"]
-                microbatch_end = macrobatch_start + microbatch_size * (k + 1) * hyperparams["group_size"]
+            n_microbatches = hyperparams["train_batch_size"] // microbatch_size
+            for k in range(n_microbatches):
+                microbatch_start = macrobatch_start + n_microbatches * k * hyperparams["group_size"]
+                microbatch_end = macrobatch_start + n_microbatches * (k + 1) * hyperparams["group_size"]
                 texts_microbatch = texts_flattened[microbatch_start: microbatch_end]
                 answers_microbatch = answers_flattened[microbatch_start: microbatch_end]
-                breakpoint()
+                input_ids_microbatch = input_ids[microbatch_start: microbatch_end]
+                labels_microbatch = labels[microbatch_start: microbatch_end]
                 old_log_probs_dict = get_response_log_probs(model=model,
-                                                            input_ids=input_ids[microbatch_start: microbatch_end],
-                                                            labels=labels[microbatch_start: microbatch_end],
+                                                            input_ids=input_ids_microbatch,
+                                                            labels=labels_microbatch,
                                                             return_token_entropy=True,
                                                             with_grad=False,
                                                             device=device)
@@ -99,7 +100,7 @@ def train_one_epoch(model,
                 breakpoint()
                 log_probs_dict = get_response_log_probs(model=model,
                                                         input_ids=input_ids_microbatch,
-                                                        labels=label_ids_microbatch,
+                                                        labels=labels_microbatch,
                                                         return_token_entropy=True,
                                                         with_grad=True)
                 policy_log_probs = log_probs_dict["log_probs"]
@@ -109,7 +110,7 @@ def train_one_epoch(model,
                                                        loss_type=hyperparams["loss_type"],
                                                        raw_rewards=raw_rewards,
                                                        advantages=advantages,
-                                                       old_log_probs=old_log_probs[microbatch_start: microbatch_end],
+                                                       old_log_probs=old_log_probs,
                                                        cliprange=hyperparams["cliprange"])
                 breakpoint()
 
